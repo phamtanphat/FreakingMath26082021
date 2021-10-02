775ba7389df98e71bc0fa259861fa0babb1c4737
phamtanphat <phatdroid94@gmail.com>
2021-10-02 21:16:54 +0700

    xu ly cho viec chon ket qua

--- app/src/main/java/com/example/freakingmath26082021/MainActivity.java
+++ app/src/main/java/com/example/freakingmath26082021/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.freakingmath26082021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -20,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     int mSt1, mSt2, mResult;
     boolean mIsTrue;
     String mCalculate;
-
+    int mScore = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +36,44 @@ public class MainActivity extends AppCompatActivity {
         mImgTrue = findViewById(R.id.imageButtonTrue);
         mImgFalse = findViewById(R.id.imageButtonFalse);
 
+        randomCalculate();
+
+        mImgTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mIsTrue){
+                    mScore += 1;
+                    mTvScore.setText(mScore +"");
+                    Toast.makeText(MainActivity.this, "Chính xác!!!", Toast.LENGTH_SHORT).show();
+                    randomCalculate();
+                }else{
+                    mScore = 0;
+                    mTvScore.setText(mScore +"");
+                    Toast.makeText(MainActivity.this, "Sai rồi!!!", Toast.LENGTH_SHORT).show();
+                    randomCalculate();
+                }
+            }
+        });
+
+        mImgFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mIsTrue){
+                    mScore += 1;
+                    mTvScore.setText(mScore +"");
+                    Toast.makeText(MainActivity.this, "Chính xác!!!", Toast.LENGTH_SHORT).show();
+                    randomCalculate();
+                }else{
+                    mScore = 0;
+                    mTvScore.setText(mScore +"");
+                    Toast.makeText(MainActivity.this, "Sai rồi!!!", Toast.LENGTH_SHORT).show();
+                    randomCalculate();
+                }
+            }
+        });
+
+    }
+    private void randomCalculate(){
         // random so thu1 va so thu2
         mRandom = new Random();
         mSt1 = mRandom.nextInt(10) + 1;
@@ -75,6 +116,10 @@ public class MainActivity extends AppCompatActivity {
         mTvCalculate.setText(mSt1 + mCalculate + mSt2);
         mTvResult.setText("= " + mResult);
 
-
+        // random ma mau
+        int red = mRandom.nextInt(255) + 1;
+        int green = mRandom.nextInt(255) + 1;
+        int blue = mRandom.nextInt(255) + 1;
+        mRelativeContainer.setBackgroundColor(Color.rgb(red,green,blue));
     }
 }
